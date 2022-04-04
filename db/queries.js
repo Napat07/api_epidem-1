c9fb53a9cebacffd0212e69b90aad3567bef5eaa
napat07 <naphat1811@gmail.com>
2022-04-04 12:21:57 +0700

    add labs

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,5 @@
+const { raw } = require('express')
+
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
@@ -8,9 +10,9 @@ const pool = new Pool({
 })
 
 const epidem_hospital = (req, res) => {
-  const pid = req.params.pid
   let query = `
-    
+    SELECT tp.patient_pid 
+    FROM t_patient tp
   
   `
   pool.query(query, (error, results) => {
@@ -43,34 +45,34 @@ const epidem_person = (req, res) => {
   const pid = req.params.pid
   let query = `
   SELECT 
-  tp.patient_pid as cid,  
-  patient_prefix_description as prefix,
-  tp.patient_firstname  as first_name , 
-  tp.patient_lastname as last_name, 
-  f4.patient_nation_description as nationality, 
-  tp.f_sex_id as gender,
-  tp.patient_birthday as birth_date,
-  EXTRACT(YEAR FROM AGE(CONCAT(date_part('year',CURRENT_DATE) + 543, '-',to_char(CURRENT_DATE, 'MM'), '-',to_char(CURRENT_DATE, 'DD')) ::date, tp.patient_birthday::date)) AS age_y,
-  EXTRACT(MONTH FROM AGE(CONCAT(date_part('year',CURRENT_DATE) + 543, '-',to_char(CURRENT_DATE, 'MM'), '-',to_char(CURRENT_DATE, 'DD')) ::date, tp.patient_birthday::date)) AS age_m, 
-  EXTRACT(DAY FROM AGE(CONCAT(date_part('year',CURRENT_DATE) + 543, '-',to_char(CURRENT_DATE, 'MM'), '-',to_char(CURRENT_DATE, 'DD')) ::date, tp.patient_birthday::date)) AS age_d,
-  tp.f_patient_marriage_status_id as marital_status_id,
-  tp.patient_house as address,
-  tp.patient_moo as moo, 
-  tp.patient_road as road, 
-  f1.address_tambon_type as tmb_code, 
-  f2.address_amphur_id as amp_code, 
-  f3.address_changwat_id as chw_code,
-  tp.patient_patient_mobile_phone as mobile_phone, 
-  tp.f_patient_occupation_id as occupation
+          tp.patient_pid as cid,  
+          patient_prefix_description as prefix,
+          tp.patient_firstname  as first_name , 
+          tp.patient_lastname as last_name, 
+          f4.patient_nation_description as nationality, 
+          tp.f_sex_id as gender,
+          tp.patient_birthday as birth_date,
+          EXTRACT(YEAR FROM AGE(CONCAT(date_part('year',CURRENT_DATE) + 543, '-',to_char(CURRENT_DATE, 'MM'), '-',to_char(CURRENT_DATE, 'DD')) ::date, tp.patient_birthday::date)) AS age_y,
+          EXTRACT(MONTH FROM AGE(CONCAT(date_part('year',CURRENT_DATE) + 543, '-',to_char(CURRENT_DATE, 'MM'), '-',to_char(CURRENT_DATE, 'DD')) ::date, tp.patient_birthday::date)) AS age_m, 
+          EXTRACT(DAY FROM AGE(CONCAT(date_part('year',CURRENT_DATE) + 543, '-',to_char(CURRENT_DATE, 'MM'), '-',to_char(CURRENT_DATE, 'DD')) ::date, tp.patient_birthday::date)) AS age_d,
+          tp.f_patient_marriage_status_id as marital_status_id,
+          tp.patient_house as address,
+          tp.patient_moo as moo, 
+          tp.patient_road as road, 
+          f1.address_tambon_type as tmb_code, 
+          f2.address_amphur_id as amp_code, 
+          f3.address_changwat_id as chw_code,
+          tp.patient_patient_mobile_phone as mobile_phone, 
+          tp.f_patient_occupation_id as occupation
 
   
-FROM t_patient tp
-inner join f_patient_prefix on tp.f_patient_prefix_id = f_patient_prefix.f_patient_prefix_id
-inner join f_address f1 on tp.patient_tambon = f1.f_address_id
-inner join f_address f2 on tp.patient_amphur = f2.f_address_id
-inner join f_address f3 on tp.patient_changwat = f3.f_address_id
-inner join f_patient_nation f4 on tp.f_patient_nation_id = f4.f_patient_nation_id
-where patient_pid = '${pid}';
+    FROM t_patient tp
+        inner join f_patient_prefix on tp.f_patient_prefix_id = f_patient_prefix.f_patient_prefix_id
+        inner join f_address f1 on tp.patient_tambon = f1.f_address_id
+        inner join f_address f2 on tp.patient_amphur = f2.f_address_id
+        inner join f_address f3 on tp.patient_changwat = f3.f_address_id
+        inner join f_patient_nation f4 on tp.f_patient_nation_id = f4.f_patient_nation_id
+        where patient_pid = '${pid}';
     `
 
   pool.query(query, (error, results) => {
@@ -120,27 +122,27 @@ const epidem_report = (req, res) => {
   let query = `
   
 
+  
+
    `
   pool.query(query, (error, results) => {
     if (error) {
       throw error
     }
-    let raw_result = results.rows
-    let result_total = []
+    let raw_result = results.rows[0]
     let message = new Object ; 
     if (raw_result.length > 0) {
-      for (let value of raw_result) {
         let raw = {
          
-        }
-        result_total.push(raw)
+
+
       }
       message.msg = true 
-      message.data = result_total
+      message.data = raw
     }
     else {
       message.msg = false 
-      message.data = result_total
+    message.data = {}
     }
     res.status(200).json(message)
   })
@@ -149,30 +151,55 @@ const epidem_report = (req, res) => {
 const epidem_labs_report = (req, res) => {
   const pid = req.params.pid
   let query = `
-  
+  SELECT	
+      trl.result_lab_name as epidem_lab_confirm_type_id,
+      substring(trl.record_date_time,1,10) as lab_report_date,
+      max(case when trim(lower(trl.result_lab_name)) like '%rna%'  then result_lab_value else '' end) as lab_report_result,
+      substring(tv.visit_begin_visit_time,1,10) as specimen_date, 
+      tv.visit_dx AS lab_his_ref_name
+
+    FROM t_patient tp
+			INNER JOIN t_visit tv ON (tp.t_patient_id = tv.t_patient_id )
+			INNER JOIN t_result_lab trl ON (tv.t_visit_id = trl.t_visit_id and trl.result_lab_value <> '' )
+			AND (   trim(lower(trl.result_lab_name)) like '%rna%' or  trim(lower(trl.result_lab_name)) like '%sputum%' )
+		
+    WHERE  	patient_pid = '${pid}'
+				    and	trl.result_lab_name like 'sputum PCR for COVID%' or trl.result_lab_name like 'SARS-CoV-2-RNA%' or trl.result_lab_name like '%rna%'
+						and (trl.result_lab_value like 'SARS-CoV-2-RNA%'  or trl.result_lab_value like 'Pos%' or trl.result_lab_value like 'DE%')
+									
+    GROUP BY 
+        tv.visit_begin_visit_time,
+        trl.record_date_time,
+        trl.result_lab_name,
+        tv.visit_dx 
+    ORDER BY
+        lab_report_date DESC
      `
 
   pool.query(query, (error, results) => {
     if (error) {
       throw error
     }
-    let raw_result = results.rows
-    let result_total = []
+    let raw_result = results.rows[0]
     let message = new Object ; 
-    if (raw_result.length > 0) {
-      for (let value of raw_result) {
+    if (raw_result) {
         let raw = {
-        
-        
-        }
-        result_total.push(raw)
+          epidem_lab_confirm_type_id : raw_result.epidem_lab_confirm_type_id,
+          lab_report_date: raw_result.lab_report_date,
+          lab_report_result: raw_result.lab_report_result,
+          specimen_date: raw_result.specimen_date,
+          specimen_place_id: "11402",
+          tests_reason_type_id: '-',
+          lab_his_ref_code: '-',
+          lab_his_ref_name: raw_result.lab_his_ref_name,
+          tmlt_code: '-'
       }
       message.msg = true 
-      message.data = result_total
+      message.data = raw
     }
     else {
       message.msg = false 
-      message.data = result_total
+      message.data = {}
     }
     res.status(200).json(message)
   })
@@ -181,8 +208,17 @@ const epidem_labs_report = (req, res) => {
 const epidem_vaccination = (req, res) => {
   const pid = req.params.pid
   let query = `
- 
- 
+  SELECT  
+      substring(tv.visit_begin_visit_time,1,10) as vaccine_date,
+      SPLIT_PART(tv.visit_dx, 'เข็ม', 1) as vaccine_manufacturer,
+      SPLIT_PART(tv.visit_dx, 'เข็ม', 2) as dose
+      
+    FROM t_patient tp
+    INNER JOIN t_visit tv 
+          ON tp.t_patient_id = tv.t_patient_id	
+
+    WHERE tv.visit_dx like '_accine C%'
+    and patient_pid = '${pid}'
   `
   pool.query(query, (error, results) => {
     if (error) {
@@ -194,7 +230,10 @@ const epidem_vaccination = (req, res) => {
     if (raw_result.length > 0) {
       for (let value of raw_result) {
         let raw = {
-       
+          vaccine_hospital_code : "11402",
+          vaccine_date : value.vaccine_date,
+          dose : value.dose,
+          vaccine_manufacturer : value.vaccine_manufacturer
 
 
         }
