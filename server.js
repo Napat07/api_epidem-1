402a5f98036652ba8da77d4ac0fbdeeaf6aa8f42
napat07 <naphat1811@gmail.com>
2022-04-08 10:54:24 +0700

    change name api

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(cors())
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/api/epidem_hospital', db.epidem_hospital)
-app.get('/api/epidem_person/:pid', db.epidem_person)
-app.get('/api/epidem_report/:pid', db.epidem_report)
-app.get('/api/epidem_labs_report/:pid', db.epidem_labs_report)
-app.get('/api/epidem_vaccination/:pid', db.epidem_vaccination)
+app.get('/api/epidem_v1/epidem_hospital', db.epidem_hospital)
+app.get('/api/epidem_v1/epidem_person/:pid', db.epidem_person)
+app.get('/api/epidem_v1/epidem_report/:pid', db.epidem_report)
+app.get('/api/epidem_v1/epidem_labs_report/:pid', db.epidem_labs_report)
+app.get('/api/epidem_v1/epidem_vaccination/:pid', db.epidem_vaccination)
 
 app.get('/api/v1/test', (req, res) => {
     res.status(200).send({
